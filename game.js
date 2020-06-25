2e7559a72f12ce6345229b84acf655a5246ff219
Joseph <jonakhle@icloud.com>
2020-06-25 19:51:43 +0400

    Added button animation on press

--- game.js
+++ game.js
@@ -1,8 +1,36 @@
 //Generate Random Color pattern
 const buttonColours = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
+const userClickedPattern = [];
+
+$(".btn").on("click", function () {
+  let userChosenColour = this.id;
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
+
+console.log(userClickedPattern);
+
 function nextSequence() {
   const randomNumber = Math.floor(Math.random() * 4);
-  const randomChosenColor = buttonColours[nextSequence()];
-  gamePattern.push(randomChosenColor);
+  const randomChosenColour = buttonColours[nextSequence()];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  let audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $("." + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("." + currentColour).removeClass("pressed");
+  }, 100);
 }
